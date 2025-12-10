877bb44623186e45b429a07259777113d8da7718
Hloriia Tsizdyn Balabanova <gloriatsizdyn@gmail.com>
2025-12-10 19:08:31 +0200

    додала і пофіксила код згідно з моїм завданням

--- UI_WinForms/UI_WinForms/MyForm.h
+++ UI_WinForms/UI_WinForms/MyForm.h
@@ -1,59 +1,164 @@
 #pragma once
 
-namespace UIWinForms {
+#include <windows.h>
+#include <vcclr.h>
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+namespace controller {
 
-	/// <summary>
-	/// Summary for MyForm
-	/// </summary>
-	public ref class MyForm : public System::Windows::Forms::Form
-	{
-	public:
-		MyForm(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
+    using namespace System;
+    using namespace System::Windows::Forms;
+    using namespace System::Drawing;
 
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~MyForm()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+    public ref class MyForm : public Form
+    {
+    public:
+        MyForm()
+        {
+            InitializeComponent();
+        }
 
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+    protected:
+        ~MyForm()
+        {
+            if (components)
+                delete components;
+        }
 
-#pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-		}
-#pragma endregion
-	};
+    private:
+        int selectedMethod = 0;
+
+        Label^ lblTitle;
+        Button^ btnPipe;
+        Button^ btnMsg;
+        Button^ btnShm;
+        Button^ btnStart;
+        Label^ lblSelected;
+        TextBox^ txtLog;
+
+        System::ComponentModel::Container^ components;
+
+        void InitializeComponent(void)
+        {
+            this->Text = L"IPC Controller";
+            this->ClientSize = Drawing::Size(360, 520);
+            this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+            this->MaximizeBox = false;
+
+            lblTitle = gcnew Label();
+            lblTitle->Text = L"IPC Controller";
+            lblTitle->Font = gcnew Drawing::Font(L"Segoe UI", 20);
+            lblTitle->Location = Point(40, 10);
+            lblTitle->Size = Drawing::Size(300, 40);
+
+            btnPipe = gcnew Button();
+            btnPipe->Text = L"Pipes / FIFO";
+            btnPipe->Location = Point(40, 70);
+            btnPipe->Size = Drawing::Size(260, 40);
+            btnPipe->Click += gcnew EventHandler(this, &MyForm::btnPipe_Click);
+
+            btnMsg = gcnew Button();
+            btnMsg->Text = L"Message Queue";
+            btnMsg->Location = Point(40, 120);
+            btnMsg->Size = Drawing::Size(260, 40);
+            btnMsg->Click += gcnew EventHandler(this, &MyForm::btnMsg_Click);
+
+            btnShm = gcnew Button();
+            btnShm->Text = L"Shared Memory";
+            btnShm->Location = Point(40, 170);
+            btnShm->Size = Drawing::Size(260, 40);
+            btnShm->Click += gcnew EventHandler(this, &MyForm::btnShm_Click);
+
+            btnStart = gcnew Button();
+            btnStart->Text = L"Start System";
+            btnStart->Location = Point(40, 230);
+            btnStart->Size = Drawing::Size(260, 40);
+            btnStart->Click += gcnew EventHandler(this, &MyForm::btnStart_Click);
+
+            lblSelected = gcnew Label();
+            lblSelected->Text = L"Selected: none";
+            lblSelected->Location = Point(40, 290);
+            lblSelected->Size = Drawing::Size(260, 20);
+
+            txtLog = gcnew TextBox();
+            txtLog->Location = Point(40, 320);
+            txtLog->Size = Drawing::Size(260, 160);
+            txtLog->Multiline = true;
+            txtLog->ReadOnly = true;
+            txtLog->ScrollBars = ScrollBars::Vertical;
+
+            this->Controls->Add(lblTitle);
+            this->Controls->Add(btnPipe);
+            this->Controls->Add(btnMsg);
+            this->Controls->Add(btnShm);
+            this->Controls->Add(btnStart);
+            this->Controls->Add(lblSelected);
+            this->Controls->Add(txtLog);
+        }
+
+        // ====== IPC selection ======
+
+        void btnPipe_Click(Object^ sender, EventArgs^ e)
+        {
+            selectedMethod = 1;
+            lblSelected->Text = L"Selected: Pipes";
+        }
+
+        void btnMsg_Click(Object^ sender, EventArgs^ e)
+        {
+            selectedMethod = 2;
+            lblSelected->Text = L"Selected: Message Queue";
+        }
+
+        void btnShm_Click(Object^ sender, EventArgs^ e)
+        {
+            selectedMethod = 3;
+            lblSelected->Text = L"Selected: Shared Memory";
+        }
+
+        // ====== Start processes ======
+
+        void btnStart_Click(Object^ sender, EventArgs^ e)
+        {
+            if (selectedMethod == 0)
+            {
+                txtLog->AppendText("ERROR: Select method first!\r\n");
+                return;
+            }
+
+            array<String^>^ procs = {
+                "client_pipe.exe",
+                "client_msg.exe",
+                "logger.exe"
+            };
+
+            for each(String ^ exe in procs)
+            {
+                String^ cmd = exe + " " + selectedMethod.ToString();
+                pin_ptr<const wchar_t> wcmd = PtrToStringChars(cmd);
+
+                STARTUPINFO si = { sizeof(si) };
+                PROCESS_INFORMATION pi;
+
+                BOOL ok = CreateProcessW(
+                    nullptr,
+                    (LPWSTR)wcmd,
+                    nullptr, nullptr,
+                    FALSE, 0,
+                    nullptr, nullptr,
+                    &si, &pi
+                );
+
+                if (ok)
+                {
+                    txtLog->AppendText("Launched: " + exe + "\r\n");
+                    CloseHandle(pi.hProcess);
+                    CloseHandle(pi.hThread);
+                }
+                else
+                {
+                    txtLog->AppendText("FAILED: " + exe + "\r\n");
+                }
+            }
+        }
+    };
 }
